cbbecb97b7a2a86bb215d606636a3e2fb8f7b2ed
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2021-12-08 16:15:12 -0800

    Se agrego el proceso de festival musica

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -1,6 +1,14 @@
-const { src,dest,watch} = require('gulp');
-const sass = require('gulp-sass')(require('sass'))
+const { src,dest,watch,parallel} = require('gulp');
+
+//CSS
+const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+//Imagenes 
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){    
     //src('src/scss/app.scss')//Identificar el archivo .scss a compilar
@@ -12,11 +20,57 @@ function css(done){
     done();
 }
 
+function imagenes(done){
+    const opciones = {
+        optimizationLevel:3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    done();
+}
+
+
+function versionWebp(done){
+
+    const opciones = {
+        quality:50
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+    done();
+}
+
+function versionAvif(done){
+
+    const opciones = {
+        quality:50
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'));
+    done();
+}
+
+function javascript(done){
+    src('src/js/**/*.js')
+        .pipe(dest('build/js'))
+    done();
+}
+
 function dev(done){
     //watch('src/scss/app.scss',css);//que archivo va a estar escuchando los cambios y que tarea le voy a dar
     watch('src/scss/**/*.scss',css); // que permita escuchar todos los archivos
+    watch('src/js/**/*.js',javascript); // que permita escuchar todos los archivos
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes,versionWebp,versionAvif,javascript,dev);
